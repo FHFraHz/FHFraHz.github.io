87ee7f9ae53a0fbb305bb5b67d17483bc4e9e2df
FHFraHz <fh.franhernandez.92@outlook.com>
2025-12-28 05:02:53 -0300

    Adding new stuff to CSS art gallery

--- js/pages/web.js
+++ js/pages/web.js
@@ -1,3 +1,11 @@
 'use strict';
 if(typeof(currentPage) === 'undefined')
     var currentPage = 'web';
+
+loadPage();
+
+async function loadPage() {
+    await yieldComponentIntoElement('components/web/css-gallery.html', 'css-gallery');
+    initializeThemeScripts();
+}
+
