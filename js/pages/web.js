2abd0388182ed6af3b728e941e1a0410f351adc3
FHFraHz <fh.franhernandez.92@outlook.com>
2026-03-15 01:28:06 -0300

    Modularized the art gallery a bit more. TODO: add more things to the 3D house scene

--- js/pages/web.js
+++ js/pages/web.js
@@ -6,6 +6,17 @@ loadPage();
 
 async function loadPage() {
     await yieldComponentIntoElement('components/web/css-gallery.html', 'css-gallery');
+    await initializeArtGallery();
     initializeThemeScripts();
 }
 
+async function yieldArtIntoGalleryFrame(cssGalleryComponentFileName, elementId) {
+    await yieldComponentIntoElement('components/web/css-gallery/'+cssGalleryComponentFileName, elementId);
+}
+
+async function initializeArtGallery() {
+    await yieldArtIntoGalleryFrame('sunny-beach.html', 'picture_frame-sunny_beach');
+    await yieldArtIntoGalleryFrame('rainy-forest.html', 'picture_frame-rainy_forest');
+    await yieldArtIntoGalleryFrame('drag-race.html', 'picture_frame-drag_race');
+    await yieldArtIntoGalleryFrame('house.html', 'picture_frame-house');
+}
